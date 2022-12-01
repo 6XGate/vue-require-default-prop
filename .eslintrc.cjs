a2e3e41796f0ccf216806175bd2e3eed78578f69
Matthew Holder <sixxgate@hotmail.com>
2022-12-01 10:53:43 -0600

    Fixed ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,37 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/strict",
-    "@vue/eslint-config-typescript/recommended"
+    'eslint:recommended'
   ],
   overrides: [
     {
-      files: ["*.ts", "*.tsx", "*.vue"],
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict'
+      ],
       parserOptions: {
-        project: './tsconfig.json'
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue']
+      }
+    },
+    {
+      files: ['*.vue'],
+      extends: [
+        'plugin:vue/recommended',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict',
+        '@vue/eslint-config-typescript/recommended'
+      ],
+      parser: "vue-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue']
       }
     }
   ]
